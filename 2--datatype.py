e8c6e177393e2668f0137b087013d859c3185a75
PARISE SRIVATSAL <srivatsalparise4831@gmail.com>
2024-04-02 17:19:29 +0000

    updates 2--variable again

--- 2--datatype.py
+++ 2--datatype.py
@@ -2,7 +2,7 @@ string = "Sri Vatsal"
 print(string.upper())
 print(string.lower())
 
-print(string.startswith('S'))
+print(string.startswith('l'))
 print(string.endswith('l'))
 print(string.find('i'))
 print(string.index('S'))
